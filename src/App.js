85d89937f1045274c6d0a8550b06c76bae6e1169
unknown <snow@ifreight.net>
2018-07-15 21:56:43 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 
 import Header from './components/Header';
 import Navi from './components/Navi';
+import Banner from './components/Banner';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Navi />
+        <div className="container">
+        <div className="row">
+        <div className="col-md-3"><Navi /></div>
+          <div className="col-md-9">
+          <Banner />
+          </div>
+          </div>
+      </div>
       </div>
     );
   }
--- src/components/Navi.js
+++ src/components/Navi.js
@@ -4,17 +4,32 @@ import './Navi.css';
 class Navi extends Component {
   render() {
     return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-3 left-menu">
+    
+        <div className=" left-menu">
           <ul>
             <li>
               <a><span className="icon-wrap"><i className="lv1-icon icon-main"></i></span> <span>Điện Thoại - Máy Tính Bảng</span></a>
+              <ul className="navibar">
+                <li>
+                  <span>Xu Hướng & Nổi Bật</span>
+                </li>
+                <li>
+                  <a>Smartphone dưới 3 triệu</a>
+                </li>
+                <li>
+                  <a>Điện thoại dưới 1 triệu</a>
+                </li>
+                <li>
+                  <a>Điện thoại dưới 1 triệu</a>
+                </li>
+                <li>
+                  <a>Điện thoại dưới 1 triệu</a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
-      </div>
-    </div>
+     
 
 );
   }
